675f4ad04bcbc23fb416a1d9ea19dc8d44b340e2
ihomba92 <ihombakibandidavid@gmail.com>
2026-05-09 13:27:54 +0300

    Update ProjectsSearch component layout and styles
    Fix input className and layout for ProjectsSearch component.

--- src/components/ProjectsSearch.jsx
+++ src/components/ProjectsSearch.jsx
@@ -2,11 +2,11 @@ export default function ProjectsSearch
 ({ search, setSearch }) {
 
   return (
-    <div className="">
+    <div className="flex justify-center w-full">
 
       <input
         className="bg-white border-b-2 border-gray-200 focus:border-amber-800 
-        transition-colors py-2 px-2 mx-auto ml-3 w-1/3 max-w-lg rounded-sm
+        transition-colors py-2 px-2 mx-auto first = useContext(second) max-w-lg rounded-sm
         outline-none font-serif italic text-lg shadow-sm"    
         type="text"
         placeholder="Search projects... 🔍"
